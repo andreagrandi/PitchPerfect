7c986099412d5c494e243addc16fb69a863ce271
Andrea Grandi <a.grandi@gmail.com>
2016-10-14 19:04:52 +0100

    Initial commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Andrea Grandi on 24/08/2016.
+//  Copyright © 2016 Andrea Grandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        print("record button was pressed")
+    }
+}
